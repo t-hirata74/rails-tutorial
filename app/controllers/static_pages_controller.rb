87d7785589529019d9aa9d5a04e68b466e891e92
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-07 16:55:11 +0900

    add comment Static Pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
+    # => "app/views/{リソース名}/@{アクション名}.html.erb"
+    # => "app/views/static_pages/home.html.erb"
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get  'static_pages/home'
+  # => StatigPages#home
+
+  get  'static_pages/help'
+  # => StaticPages#help
+
   root 'application#hello'
 end
