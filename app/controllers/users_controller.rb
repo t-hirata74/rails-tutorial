2f693e5332bf8e1735196c1ae70d9567544b3d68
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-07-16 19:36:48 +0900

    7 first half

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
   def new
+    @user = User.new
+  end
+
+  # POST /users (+ params)
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      # Success
+    else
+      # Failure
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
