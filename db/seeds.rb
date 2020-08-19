ba1b452c9f2b771c0e2dbb8e37cb4180c9093125
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-08-19 13:45:02 +0900

    11 first half

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,9 @@ User.create!(name:  "Example User",
   email: "example@railstutorial.org",
   password:              "foobar",
   password_confirmation: "foobar",
-  admin: true
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now
 )
 
 # 追加のユーザーをまとめて生成する
@@ -14,5 +16,8 @@ password = "password"
 User.create!(name:  name,
     email: email,
     password:              password,
-    password_confirmation: password)
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now
+)
 end
