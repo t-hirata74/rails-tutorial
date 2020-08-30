ee2ed7d772b48617cfbdd3b534c410e7adb8184d
Tomoaki Hirata <t.hirata.0463@gmail.com>
2020-08-30 12:55:54 +0900

    finish ch13 except ch13.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   # GET /users/:id
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   # GET /users/new
